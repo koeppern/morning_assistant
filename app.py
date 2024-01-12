5f33d93a51d495cd43f84e91823f6b3105e09cbe
Johannes Köppern <johannes.koeppern@googlemail.com>
2024-01-12 10:57:38 +0100

    added resources.yml

--- app.py
+++ app.py
@@ -6,6 +6,9 @@ Start with dummy data from HardcodedPersonalDataHub clas.
 """
 from classes.personal_data_hub import HardcodedPersonalDataHub as DataHub
 from classes.data_extractor import DataExtractor
+from dotenv import load_dotenv, find_dotenv
+import os
+import yaml
 
 
 # -------------------------------------------------------
@@ -15,13 +18,43 @@ control_parameters = {
     "print_data_hub": True,
 }
 
+
 # -------------------------------------------------------
 ## Functions
 # -------------------------------------------------------
+def load_yaml_file(path):
+    with open(path, "r") as file:
+        try:
+            return yaml.safe_load(file)
+        except yaml.YAMLError as exc:
+            print(exc)
+
+
+def propare_environment():
+    load_dotenv(find_dotenv(".env_morning_assistant"))
+
+    BASE_DIR = os.getenv("BASE_DIR")
+
+    RESOURCES_FILE_NAME = os.getenv("RESOURCES_FILE_NAME")
+
+    os.chdir(BASE_DIR)
+
+    resources_file_name = os.path.join(BASE_DIR, RESOURCES_FILE_NAME)
+
+    resources = load_yaml_file(resources_file_name)
+
+    return resources
+
 
 # -------------------------------------------------------
 ## App
 # -------------------------------------------------------
+# Load environmaent and configuration variables ------------------------------------------------------------------
+resources = propare_environment()
+
+# load yml file config_file_path
+
+
 # Object data_hub, update data ------------------------------------------------------------------
 data_hub = DataHub()
 
@@ -30,5 +63,5 @@ data_hub.update_data()
 if control_parameters["print_data_hub"]:
     data_hub.print_data()
 
-# Extract summary ------------------------------------------------------------------
+# Create summary ------------------------------------------------------------------
 extractor = DataExtractor(data_hub)
